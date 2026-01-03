0802b250f6b93d68fc281e5be6ad2f0e6767897a
Ondřej Mekina <mekina@duck.com>
2026-01-03 19:21:21 +0100

    print just debug info in dtq

--- dtq/src/main.rs
+++ dtq/src/main.rs
@@ -1,30 +1,13 @@
-use std::io::{BufWriter, Write, stdout};
-
 use clap::Parser;
+use dtparse::parse;
 
 #[derive(Parser)]
 struct Args {
     input_file: std::path::PathBuf,
 }
 
-fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let args = Args::parse();
 
-    let mut file_streamer = dtparse::BasicFileStreamer::new(&args.input_file)?;
-    let mut raw_tracker = dtparse::RawPointerTracker::new(&mut file_streamer);
-    let mut string_decoder = dtparse::StringDecoder::new(&mut raw_tracker);
-    let tracker = dtparse::PointerTracker::new(&mut string_decoder, args.input_file);
-    let res = match tracker.collect::<Result<Result<String, _>, _>>().unwrap() {
-        Ok(v) => v,
-        Err(report) => {
-            let mut stdout = BufWriter::new(stdout());
-            let mut reader = dtparse::BasicFileReader::default();
-            dtparse::ReportDisplay::new(&*report).write(&mut reader, &mut stdout)?;
-            stdout.flush()?;
-            std::process::exit(1);
-        }
-    };
-    println!("ok: {:?}", res);
-
-    Ok(())
+    println!("{:?}", parse(&args.input_file));
 }
