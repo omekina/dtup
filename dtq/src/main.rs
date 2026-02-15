eb29b4d4577ebae28ca3238aea994280f51b3a69
Ondřej Mekina <mekina@duck.com>
2026-02-15 18:15:39 +0100

    update dtq

--- dtq/src/main.rs
+++ dtq/src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use dtparse::{BasicFileReader, ParseErrorReport, ReportDisplay, StreamResult, parse};
+use dtparse::{BasicFileReader, ParseErrorReport, ReportDisplay, parse};
 use std::io::{BufWriter, Stdout, Write, stdout};
 
 #[derive(Parser)]
@@ -20,9 +20,8 @@ fn main() {
     let mut stdout = BufWriter::new(stdout());
     let start_time = std::time::Instant::now();
     let (tokens, reports) = match parse(&args.input_file) {
-        StreamResult::Ok(v) => v,
-        StreamResult::IoError(e) => panic!("io error: {e:?}"),
-        StreamResult::ProcessingError(()) => panic!(),
+        Ok(v) => v,
+        Err(e) => panic!("io error: {e:?}"),
     };
     let execution_time = start_time.elapsed();
     for report in reports {
